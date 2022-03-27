243fb608f7be9b90fcd7c6b128d126bf8a624e3b
Mahmoud Ali <muddibr@gmail.com>
2022-03-27 12:34:40 -0300

    Commit inicial

---
+++ Notion-Madddas/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Notion.Client;
+
+var client = NotionClientFactory.Create(new ClientOptions
+{
+    AuthToken = Environment.GetEnvironmentVariable("AuthToken")
+});
+
+var pages = await client.Databases.QueryAsync(Environment.GetEnvironmentVariable("DatabaseId"), new DatabasesQueryParameters());
+// var itensCardapio = pages.Properties.Where(x => x.Key == "Cardápio Letticia" || x.Key == "Cardapio Mud")
+//     .Select(x => x.Value).OfType<MultiSelectProperty>().ToList();
+
+Console.ReadLine();
