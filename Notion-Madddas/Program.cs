bcea2f12acb01bee55a98e2c7224a6f0db630714
Letticia Nicoli <letticia.nicoli@gmail.com>
2022-03-27 15:09:02 -0300

    Lógica resultado busca

--- Notion-Madddas/Program.cs
+++ Notion-Madddas/Program.cs
@@ -14,14 +14,22 @@ ConsoleDebugger.Imprimir(cardápio);
 
 using var playwright = await Playwright.CreateAsync();
 await using var browser =
-    await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = false, SlowMo = 200 });
+    await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = false, SlowMo = 1_000 });
 var context = await browser.NewContextAsync(new BrowserNewContextOptions { BaseURL = "https://maddas.com.br/" });
 var page = await context.NewPageAsync();
 
 foreach (var item in cardápio.Itens)
 {
     await page.GotoAsync($"busca?palavra={item.Porção.Nome}");
-    await page.ClickAsync("text=Adicionar ao carrinho");
+
+    var resultados = await page.Locator(".box-produto").CountAsync();
+    
+    if(resultados == 1)
+        await page.ClickAsync("text=Adicionar ao carrinho");
+    else
+    {
+        // await page.Locator($"{item.Porção.Nome}");
+    }
     await page.ClickAsync("text=Continuar comprando");
 }
 
