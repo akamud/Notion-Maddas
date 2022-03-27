ec94a39e7875a197a349ae0182bda0c13d79116c
Mahmoud Ali <muddibr@gmail.com>
2022-03-27 14:51:41 -0300

    Adiciona playwright

--- Notion-Madddas/Program.cs
+++ Notion-Madddas/Program.cs
@@ -1,4 +1,5 @@
-﻿using NotionMaddas;
+﻿using Microsoft.Playwright;
+using NotionMaddas;
 using NotionMaddas.Dominio;
 
 var porções = await NotionParser.ObterPorções();
@@ -10,5 +11,18 @@ foreach (var porção in porções)
 }
 
 ConsoleDebugger.Imprimir(cardápio);
+
+using var playwright = await Playwright.CreateAsync();
+await using var browser =
+    await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = false, SlowMo = 200 });
+var context = await browser.NewContextAsync(new BrowserNewContextOptions { BaseURL = "https://maddas.com.br/" });
+var page = await context.NewPageAsync();
+
+foreach (var item in cardápio.Itens)
+{
+    await page.GotoAsync($"busca?palavra={item.Porção.Nome}");
+    await page.ClickAsync("text=Adicionar ao carrinho");
+    await page.ClickAsync("text=Continuar comprando");
+}
 
 Console.ReadLine();
