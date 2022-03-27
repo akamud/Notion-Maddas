e0f0944b2344f720d1889b9457a00b8fea99be14
Letticia Nicoli <letticia.nicoli@gmail.com>
2022-03-27 13:00:34 -0300

    Filtra itens do cardapio

--- Notion-Madddas/Program.cs
+++ Notion-Madddas/Program.cs
@@ -7,8 +7,15 @@ var client = NotionClientFactory.Create(new ClientOptions
     AuthToken = Environment.GetEnvironmentVariable("AuthToken")
 });
 
-var pages = await client.Databases.QueryAsync(Environment.GetEnvironmentVariable("DatabaseId"), new DatabasesQueryParameters());
+var pages = await client.Databases.QueryAsync(Environment.GetEnvironmentVariable("DatabaseId"),
+    new DatabasesQueryParameters());
 // var itensCardapio = pages.Properties.Where(x => x.Key == "Cardápio Letticia" || x.Key == "Cardapio Mud")
 //     .Select(x => x.Value).OfType<MultiSelectProperty>().ToList();
+var keyValuePairs = pages.Results.SelectMany(x => x.Properties)
+    .Where(x => x.Key == "Cardápio Letticia" || x.Key == "Cardapio Mud")
+    .Select(x => x.Value).OfType<MultiSelectPropertyValue>()
+    .SelectMany(value => value.MultiSelect)
+    .Select(x => x.Name);
+
 
 Console.ReadLine();
