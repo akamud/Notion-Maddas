4471b6f94ce882ed603ea604e3c820c50056cda0
Letticia Nicoli <letticia.nicoli@gmail.com>
2022-03-27 17:48:34 -0300

    WIP

--- Notion-Madddas/Program.cs
+++ Notion-Madddas/Program.cs
@@ -1,21 +1,29 @@
 ﻿using NotionMaddas;
 using NotionMaddas.Dominio;
+using Spectre.Console;
 
-var porções = await NotionParser.ObterPorções();
+await AnsiConsole.Progress()
+    .Columns(new TaskDescriptionColumn(), new ProgressBarColumn(), new SpinnerColumn())
+    .StartAsync(async contexto =>
+    {
+        var notionTask = contexto.AddTask("Obtendo dados do Notion").IsIndeterminate();
+        var porções = await NotionParser.ObterPorções();
+        notionTask.StopTask();
 
-var cardápio = new Cardápio();
-foreach (var porção in porções)
-{
-    cardápio.AdicionarItem(porção);
-}
+        var cardápio = new Cardápio();
+        foreach (var porção in porções)
+        {
+            cardápio.AdicionarItem(porção);
+        }
+        
+        ConsoleDebugger.Imprimir(cardápio);
 
-ConsoleDebugger.Imprimir(cardápio);
+        Console.WriteLine("É HORA DO SHOW");
 
-Console.WriteLine("É HORA DO SHOW");
+        AutomatizadorCompra automatizadorCompra = new();
+        await automatizadorCompra.ExecutarCompra(cardápio, false);
 
-AutomatizadorCompra automatizadorCompra = new();
-await automatizadorCompra.ExecutarCompra(cardápio, false);
-
-Console.WriteLine("E POR HOJE É SÓ");
+        Console.WriteLine("E POR HOJE É SÓ");
+    });
 
 Console.ReadLine();
