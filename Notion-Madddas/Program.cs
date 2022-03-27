abe8dc7927f63ec7ebc1989bd2c386ca1da1ecd6
Mahmoud Ali <muddibr@gmail.com>
2022-03-27 17:51:14 -0300

    WIP

--- Notion-Madddas/Program.cs
+++ Notion-Madddas/Program.cs
@@ -2,28 +2,33 @@
 using NotionMaddas.Dominio;
 using Spectre.Console;
 
+Cardápio? cardápio = null;
 await AnsiConsole.Progress()
     .Columns(new TaskDescriptionColumn(), new ProgressBarColumn(), new SpinnerColumn())
     .StartAsync(async contexto =>
     {
         var notionTask = contexto.AddTask("Obtendo dados do Notion").IsIndeterminate();
         var porções = await NotionParser.ObterPorções();
+        notionTask.Increment(1);
         notionTask.StopTask();
 
-        var cardápio = new Cardápio();
+        cardápio = new Cardápio();
         foreach (var porção in porções)
         {
             cardápio.AdicionarItem(porção);
         }
-        
-        ConsoleDebugger.Imprimir(cardápio);
-
-        Console.WriteLine("É HORA DO SHOW");
-
-        AutomatizadorCompra automatizadorCompra = new();
-        await automatizadorCompra.ExecutarCompra(cardápio, false);
-
-        Console.WriteLine("E POR HOJE É SÓ");
     });
+
+if (cardápio == null)
+    return;
+
+ConsoleDebugger.Imprimir(cardápio);
+
+Console.WriteLine("É HORA DO SHOW");
+
+AutomatizadorCompra automatizadorCompra = new();
+await automatizadorCompra.ExecutarCompra(cardápio, false);
+
+Console.WriteLine("E POR HOJE É SÓ");
 
 Console.ReadLine();
