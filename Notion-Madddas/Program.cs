4c410fb3d3be22fb49ffd7c614f6e67cee2d09fd
Mahmoud Ali <muddibr@gmail.com>
2022-03-27 15:17:18 -0300

    WIP

--- Notion-Madddas/Program.cs
+++ Notion-Madddas/Program.cs
@@ -14,7 +14,10 @@ ConsoleDebugger.Imprimir(cardápio);
 
 using var playwright = await Playwright.CreateAsync();
 await using var browser =
-    await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = false, SlowMo = 1_000 });
+    await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+    {
+        Headless = false, SlowMo = 1_000, Timeout = 10_000
+    });
 var context = await browser.NewContextAsync(new BrowserNewContextOptions { BaseURL = "https://maddas.com.br/" });
 var page = await context.NewPageAsync();
 
@@ -23,13 +26,16 @@ foreach (var item in cardápio.Itens)
     await page.GotoAsync($"busca?palavra={item.Porção.Nome}");
 
     var resultados = await page.Locator(".box-produto").CountAsync();
-    
-    if(resultados == 1)
+
+    if (resultados == 1)
         await page.ClickAsync("text=Adicionar ao carrinho");
     else
     {
-        // await page.Locator($"{item.Porção.Nome}");
+        // TODO: arroz branco não está achando
+        var teste = page.Locator($"'{item.Porção.Nome}'").First;
+        // await page.Locator($"'{item.Porção.Nome}'").Locator("text=Adicionar ao carrinho").ClickAsync();
     }
+
     await page.ClickAsync("text=Continuar comprando");
 }
 
